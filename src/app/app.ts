0084b15288a20ba661835835a2046b2a4d1db579
adriansito124 <adriangobara@gmail.com>
2025-07-28 18:51:38 -0300

    records page ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Records } from './pages/records/records';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home], 
+  imports: [RouterOutlet, Records], //Home Records
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/pages/records/records.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
+
+@Component({
+  selector: 'app-records',
+  imports: [Header],
+  templateUrl: './records.html',
+  styleUrl: './records.scss'
+})
+export class Records {
+
+}
